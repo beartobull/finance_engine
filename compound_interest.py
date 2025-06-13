f9860ee264a7884c5495182ac82fe8aef1322dcb
beartobull <guptaananya1029@gmail.com>
2025-06-13 22:17:12 +0530

    updated  function to calculate future values

--- compound_interest.py
+++ compound_interest.py
@@ -1,4 +1,4 @@
 def calculate_future_value(pv,rate,years): 
 fv=pv*(rate+1/100) ** years
 print("future value after", years, "year is", round (fv,2)
-calculate_future_values(10000;5%;5)
+calculate_future_values(10000,5,5)
